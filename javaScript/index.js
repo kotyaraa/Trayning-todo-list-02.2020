4886bf02f7fa0a08fa8897641ae4270b39e6fb89
olkn <42804223+kotyaraa@users.noreply.github.com>
2020-03-27 05:55:08 +0300

    my second commit

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,14 +1,13 @@
 console.log("Hello JS");
 const todo_list = document.querySelector(".todo-list");
 
-
 const $header = document.createElement("div");
 
-$header.setAttribute("class","header"); 
+$header.setAttribute("class", "header");
 todo_list.appendChild($header);
 
 let $myheader = document.createElement("my-header");
-$myheader.setAttribute("class","my-header");
+$myheader.setAttribute("class", "my-header");
 $header.appendChild($myheader);
 
 const $title = document.createElement("h2");
@@ -22,32 +21,55 @@ $header_inp.class = "myInput";
 $header_inp.placeholder = "Title...";
 $myheader.appendChild($header_inp);
 
+const $headerBtn = document.createElement("button");
+$headerBtn.setAttribute("class", "header_button");
+const $headerBtnTxt = document.createTextNode("ADD");
+$headerBtn.appendChild($headerBtnTxt);
+$myheader.appendChild($headerBtn);
 
- const $headerBtn = document.createElement("button");
- $headerBtn.setAttribute("class","header_button");
- const $headerBtnTxt = document.createTextNode("ADD");
- $headerBtn.appendChild($headerBtnTxt);
- $myheader.appendChild($headerBtn);
 let $ul = document.createElement("ul");
 $myheader.appendChild($ul);
 
-const $BtnRemove = document.createElement("button");
-$BtnRemove.setAttribute("class","BtnRemove");
-$BtnRemoveBtnTxt1 = document.createTextNode("Remove")
-$BtnRemove.appendChild($BtnRemoveBtnTxt1);
 
-const $BtnDone = document.createElement("button");
-$BtnDone.setAttribute("class","BtnDone");
-$BtnDoneBtnTxt1 = document.createTextNode("Done")
-$BtnDone.appendChild($BtnDoneBtnTxt1);
 
-$headerBtn.addEventListener("click", (event) => {
-    var value = $header_inp.value;
-    var $li = document.createElement("li");
-    $li.innerHTML = value;  
-   $li.appendChild($BtnRemove);  
-    $li.appendChild($BtnDone);
-$ul.appendChild($li);
-    $header_inp.value = "";
-},false);
+   const headBtn = $headerBtn.addEventListener(
+    "click",
+    event => {
+      const value = $header_inp.value;
+      if(value !== ""){
+        const $BtnRemove = document.createElement("button");
+      $BtnRemove.setAttribute("class", "BtnRemove");
+      $BtnRemoveBtnTxt1 = document.createTextNode("Remove");
+      $BtnRemove.appendChild($BtnRemoveBtnTxt1);
+      
+      
+      const $BtnDone = document.createElement("button");
+      $BtnDone.setAttribute("class", "BtnDone");
+      $BtnDoneBtnTxt1 = document.createTextNode("Done");
+      $BtnDone.appendChild($BtnDoneBtnTxt1);
+      
+      const $li = document.createElement("li");
+      $li.innerHTML = value;
+      $li.appendChild($BtnRemove);
+      $li.appendChild($BtnDone);
+      $ul.appendChild($li);
+      $header_inp.value = "";
+  
+      $BtnRemove.addEventListener("click", function(){
+          $li.remove();
+      })
+      $BtnDone.addEventListener("click", ()=>{
+        $li.style.textDecoration="line-through";
+      })
+    }
+      },
 
+    false
+    
+  );
+
+  
+    
+  
+      
+    
